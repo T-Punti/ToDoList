635fc47c2475e1ee1d5409725256dd0b233e3ce7
T-Punti <72748202+T-Punti@users.noreply.github.com>
2022-11-16 09:26:48 +0100

    stand 16.11.2022

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,36 +1,47 @@
 <template>
   <div class="container">
     <CreateItem :createItem="createItem" />
+    <ToDoList :toDoList="data" :deleteData="removeItem" />
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
 import CreateItem from './CreateItem.vue';
+import ToDoList from './ToDoList.vue';
 export default {
   components: {
     CreateItem,
+    ToDoList,
   },
 
   setup() {
-    let data = ref(
+    let data = ref([
       { name: 'Eating' },
       { name: 'Drinking' },
-      { name: 'Sleeping' }
-    );
+      { name: 'Sleeping' },
+    ]);
 
     function createItem(item) {
-      //data.value.push({ name: item });
       console.log('added' + item);
+      if (!data.value.includes(item)) data.value.push({ name: item });
+    }
+    function removeItem(index) {
+      data.value.splice(index, 1);
     }
 
     return {
       data,
       createItem,
+      removeItem,
     };
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+ul {
+  list-style: none;
+}
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in toDoList" :key="index">
+        <span class="item">{{ item.name }}</span>
+        <span class="delete" @click="deleteData(index)">X</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    toDoList: {
+      type: Array,
+      required: true,
+    },
+    deleteData: {
+      type: Function,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  margin-top: 10px;
+  justify-content: center;
+}
+.delete {
+  background-color: red;
+  color: white;
+  padding: 0px 10px;
+  border: black solid;
+  border-radius: 8px;
+
+  cursor: pointer;
+}
+
+.item {
+  width: 100px;
+}
+</style>
